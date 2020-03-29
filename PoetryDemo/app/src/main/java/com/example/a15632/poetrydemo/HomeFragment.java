f705b5520490b213e5680d1f0c5eede882449fae
FuShaoLei <1563250958@qq.com>
2020-03-29 17:30:30 +0800

    稍微完成了一点drawer

--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/HomeFragment.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/HomeFragment.java
@@ -1,15 +1,24 @@
 package com.example.a15632.poetrydemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class HomeFragment extends Fragment {
     private View fragment;
+    private LinearLayout layout_to_search;
+    private ImageView home_avator;
+    private DrawerLayout drawerLayout;
+    private LinearLayout left_drawer;
 
     @Nullable
     @Override
@@ -19,7 +28,8 @@ public class HomeFragment extends Fragment {
         fragment=inflater.inflate(R.layout.tab_home_layout,container,false);
 
         //code begin
-
+        findView();
+        action();
 
 
 
@@ -33,5 +43,29 @@ public class HomeFragment extends Fragment {
         }
 
         return fragment;
+    }
+
+
+    private void findView() {
+        home_avator=fragment.findViewById(R.id.img__home_avator);
+        layout_to_search=fragment.findViewById(R.id.linear_click_to_search);
+        drawerLayout=fragment.findViewById(R.id.drawer_layout);
+        left_drawer=fragment.findViewById(R.id.left_drawer);
+    }
+    private void action() {
+        //点击后跳转到搜索界面
+        layout_to_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),Search.class));
+            }
+        });
+        //点击头像后唤出抽屉
+        home_avator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/Search.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/Search.java
@@ -2,12 +2,47 @@ package com.example.a15632.poetrydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Search extends AppCompatActivity {
+    private EditText input;
+    private TextView cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();//去掉顶部标题栏
         setContentView(R.layout.activity_search);
+
+        findView();
+        action();
+    }
+
+
+    private void findView() {
+        input=findViewById(R.id.et_home_input);
+        cancel=findViewById(R.id.tv_back_to_home);
+
+    }
+
+    private void action() {
+        //关于EditView的操作,相应虚拟键盘上的"搜索"按钮
+        input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                return false;
+            }
+        });
+        //点击取消后返回首页
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
