2ea36b95f84f2e01c18dd2601e69cad932f6a8a3
hl115 <1154453784@qq.com>
2020-04-28 10:11:58 +0800

    updatedetail

--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/AddTalkFragment.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/AddTalkFragment.java
@@ -8,16 +8,12 @@ import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.a15632.poetrydemo.Entity.Community;
 import com.example.a15632.poetrydemo.Entity.User;
@@ -29,8 +25,6 @@ import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
-
-import static android.view.Gravity.CENTER;
 
 public class AddTalkFragment extends Fragment{
 
@@ -47,16 +41,6 @@ public class AddTalkFragment extends Fragment{
         public void handleMessage(Message msg) {
             switch (msg.what){
                 case REFRESH_FINISH:
-                    User u = new User("zhsan","123446",R.drawable.default_headimg);
-                    // userList.add(u);
-                    long time=System.currentTimeMillis();
-                    Date date=new Date(time);
-                    Community c=new Community("春眠","春眠不觉晓",100,date);
-                    //修改数据源
-                    userList.add(0,u);
-                    communityList.add(0,c);
-                    //更新Adapter
-                    communityAdapter.notifyDataSetChanged();
                     //结束刷新动画
                     refreshLayout.finishRefresh();
                     break;
@@ -92,22 +76,11 @@ public class AddTalkFragment extends Fragment{
 
         return fragment;
     }
-    //初始化布局
-    private void initLayout(){
-        LayoutInflater factory = LayoutInflater.from(getContext());
-        View layout = factory.inflate(R.layout.community_list, null);
-        TextView tv=(TextView) layout.findViewById(R.id.tv_title);
-        Log.e("title",tv.getText().toString());
-        tv.setGravity(Gravity.CENTER);
-
-    }
 
     private void findViews(){
         listView =fragment.findViewById(R.id.lv_data);
-        // initLayout();
-        communityAdapter= new CommunityAdapter(fragment.getContext(),communityList,userList,R.layout.community_list,2);
+        communityAdapter= new CommunityAdapter(fragment.getContext(),communityList,userList,R.layout.community_list,1);
         listView.setAdapter(communityAdapter);
-
 
         //获取智能刷新布局
         refreshLayout = fragment.findViewById(R.id.refreshLayout);
@@ -150,7 +123,7 @@ public class AddTalkFragment extends Fragment{
         @Override
         protected Object doInBackground(Object[] objects) {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -160,19 +133,10 @@ public class AddTalkFragment extends Fragment{
         @Override
         protected void onPostExecute(Object o) {
             //更新视图
-            User u = new User("zhsan","123446",R.drawable.default_headimg);
-            userList.add(u);
-            long time=System.currentTimeMillis();
-            Date date=new Date(time);
-            Community c=new Community("春眠","春眠不觉晓",100,date);
-            communityList.add(c);
-            communityAdapter.notifyDataSetChanged();
             //结束加载更多的动画
             refreshLayout.finishLoadMore();
         }
     }
-
-
 
     //准备数据源
     private void initData(){
@@ -188,11 +152,12 @@ public class AddTalkFragment extends Fragment{
         userList.add(u4);
         long time=System.currentTimeMillis();
         Date date=new Date(time);
-        Community c=new Community("静夜思","床前明月光，疑是地上霜。举头望明月，低头思故乡。",100,100,date);
-        Community c1=new Community("惠崇春江晚景","竹外桃花三两枝，春江水暖鸭先知。蒌蒿满地芦芽短，正是河豚欲上时。",100,100,date);
-        Community c2=new Community("绝句","迟日江山丽，春风花草香。泥融飞燕子，沙暖睡鸳鸯",100,100,date);
-        Community c3=new Community("别董大","千里黄云白日曛，北风吹雁雪纷纷，。莫愁前路无知己，天下谁人不识君",100,100,date);
-        Community c4=new Community("秋浦歌","白发三千丈，缘愁似个长。不知明镜里，何处得秋霜。",100,100,date);
+
+        Community c=new Community("浅谈我对《静夜思》的一点看法","床前明月光，疑是地上霜。举头望明月，低头思故乡。",100,100,date);
+        Community c1=new Community("分析杜甫的诗的深度","床前明月光，疑是地上霜。举头望明月，低头思故乡。",100,100,date);
+        Community c2=new Community("浅谈我对《石壕吏》的一点看法","床前明月光，疑是地上霜。举头望明月，低头思故乡。",100,100,date);
+        Community c3=new Community("浅谈我对《静夜思》的一点看法","床前明月光，疑是地上霜。举头望明月，低头思故乡。",100,100,date);
+        Community c4=new Community("浅谈我对《静夜思》的一点看法","床前明月光，疑是地上霜。举头望明月，低头思故乡。",100,100,date);
         communityList.add(c);
         communityList.add(c1);
         communityList.add(c2);
--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/CommunityFragment.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/CommunityFragment.java
@@ -2,29 +2,22 @@ package com.example.a15632.poetrydemo;
 
 
 import android.app.ActionBar;
-import android.content.Intent;
-import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 
-import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-import android.widget.Button;
+
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.PopupWindow;
 import android.widget.Toast;
@@ -34,24 +27,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommunityFragment extends Fragment{
-    //选项卡中的按钮
-    private Button btn_addpoem=null;
-    private Button btn_addtalk=null;
-
-    private View cursor;
-    private int offset = 0;
-    private  int screenWidth = 0;
-    private int screenl_3;
-    private LinearLayout.LayoutParams lp;
-
-
     private  ViewPager mViewPager;
     private View fragment;
-    FragmentManager fragmentManager;
-    ViewPagerFragmentAdapter mViewPagerFragmentAdapter;
+    private TabLayout mTabLayout;
 
     private TitleLayout titleLayout;
-    private Button btn_addCommuniy=null;
     private ImageView iv_delete=null;
 
     List<Fragment> mFragmentList = new ArrayList<Fragment>();
@@ -63,30 +43,12 @@ public class CommunityFragment extends Fragment{
         fragment=inflater.inflate(R.layout.tab_community_layout,container,false);
         //code begin
         findViews();
+        initViews();
         MyListener myListener=new MyListener();
-        btn_addpoem.setOnClickListener(myListener);
-        btn_addtalk.setOnClickListener(myListener);
         //发布按钮
         titleLayout.setRightIconOnClickListener(myListener);
-
-        //页面切换
-        fragmentManager =getChildFragmentManager();//定义fragment管理器
-        initFragmetList();//初始化fragment的列表
-        mViewPagerFragmentAdapter = new ViewPagerFragmentAdapter(fragmentManager,mFragmentList);//设置viewpager的适配器
-        initViewPager();//初始化viewpager        //code end
+        //code end
         ViewGroup p=(ViewGroup)fragment.getParent();
-
-        //获取屏幕宽度
-        DisplayMetrics dm = new DisplayMetrics();
-        getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-        screenWidth = dm.widthPixels;
-        screenl_3 = screenWidth/2;  //裁剪2分之1
-        lp = (LinearLayout.LayoutParams)cursor.getLayoutParams();
-
-
-
-
-
         if(p!=null){
             p.removeView(fragment);
         }
@@ -95,95 +57,32 @@ public class CommunityFragment extends Fragment{
     }
 
     public void findViews(){
-        btn_addpoem=fragment.findViewById(R.id.btn_addpoem);
-        btn_addtalk=fragment.findViewById(R.id.btn_addtalk);
-        cursor = fragment.findViewById(R.id.cursor);
-        mViewPager = (ViewPager)fragment.findViewById(R.id.viewpager);
+        mViewPager=(ViewPager) fragment.findViewById(R.id.viewpager);
+        mTabLayout=(TabLayout) fragment.findViewById(R.id.tabLayout);
         titleLayout=fragment.findViewById(R.id.title_bar);
     }
-
-
-    public void initViewPager() {
-        mViewPager.addOnPageChangeListener(new ViewPagetOnPagerChangedLisenter());
-        mViewPager.setAdapter(mViewPagerFragmentAdapter);
-        mViewPager.setCurrentItem(0);
-        mViewPager.setOffscreenPageLimit(2);
-
-
-
-    }
-    public void initFragmetList() {
-        mFragmentList.clear();
-        Fragment addPoemFragment = new AddPoemFragment();
-        Fragment addTalkFragment=new AddTalkFragment();
-        mFragmentList.add(addPoemFragment);
-        mFragmentList.add(addTalkFragment);
-
-    }
-    private class ViewPagetOnPagerChangedLisenter implements ViewPager.OnPageChangeListener {
-
-
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            offset = (screenl_3-cursor.getLayoutParams().width)/2;
-            Log.d("TAG", "111----"+position + "--" + positionOffset + "--"
-                    + positionOffsetPixels);
-            final float scale = getResources().getDisplayMetrics().density;
-            if (position == 0){
-                lp.leftMargin = (int)(positionOffsetPixels/2)+offset;
-            }else if(position ==1){
-                lp.leftMargin = (int)(positionOffsetPixels/2)+screenl_3+offset;
-            }
-            cursor.setLayoutParams(lp);
-            upTextcolor(position);
-
-        }
-        @Override
-        public void onPageSelected(int position) {
-            Log.d("CHANGE","onPageSelected");
-        }
-        @Override
-        public void onPageScrollStateChanged(int state) { }
-    }
-
-    private void upTextcolor(int position){
-        if (position==0){
-            btn_addpoem.setTextColor(getResources().getColor(R.color.colorTheme));
-            btn_addtalk.setTextColor(getResources().getColor(R.color.colorText));
-        }else if(position==1){
-            btn_addpoem.setTextColor(getResources().getColor(R.color.colorText));
-            btn_addtalk.setTextColor(getResources().getColor(R.color.colorTheme));
-        }else if(position==2){
-            btn_addpoem.setTextColor(getResources().getColor(R.color.colorTheme));
-            btn_addtalk.setTextColor(getResources().getColor(R.color.colorText));
-        }
+    private void initViews(){
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getChildFragmentManager());
+        viewPagerAdapter.AddFragment(new AddPoemFragment(),"原创诗词");
+        viewPagerAdapter.AddFragment(new AddTalkFragment(),"社区话题");
+        mViewPager.setAdapter(viewPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
     private class MyListener implements View.OnClickListener{
         @Override
         public void onClick(View v) {
             switch(v.getId()){
-                case R.id.btn_addpoem:
-                    mViewPager.setCurrentItem(0);
-                    break;
-                case R.id.btn_addtalk:
-                    mViewPager.setCurrentItem(1);
-                    break;
                 case R.id.addcommunity:
                     // 弹出PopupMenu
                     showPopupMenu(v);
             }
         }
     }
-
     private void showPopupMenu(View v) {
-        // 1. 创建PopupMenu
         PopupMenu popupMenu
                 = new PopupMenu(fragment.getContext(), v);
-        // 2. 通过XML资源文件对PopupMenu进行填充
         popupMenu.getMenuInflater()
                 .inflate(R.menu.toolbar, popupMenu.getMenu());
-        // 3. 绑定监听器
         popupMenu.setOnMenuItemClickListener(
                 new PopupMenu.OnMenuItemClickListener() {
                     @Override
@@ -199,24 +98,21 @@ public class CommunityFragment extends Fragment{
                                 showPopupWindow(fragment);
                                 break;
                         }
-
                         return false;
                     }
                 }
         );
-        // 4. 显示PopupMenu
         popupMenu.show();
 
     }
-
     //显示弹窗
     private void showPopupWindow(View v){
         View popupWindowView=getLayoutInflater().inflate(R.layout.addcommunity,null);
         final PopupWindow popupWindow=new PopupWindow(popupWindowView, ActionBar.LayoutParams.MATCH_PARENT ,ActionBar.LayoutParams.MATCH_PARENT, true);
-        // 设置背景颜色变暗
         WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
-        lp.alpha = 0.7f;//调节透明度
+        lp.alpha = 0.7f;
         getActivity().getWindow().setAttributes(lp);
+        //dismiss时恢复原样
         popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
             public void onDismiss() {
@@ -229,6 +125,7 @@ public class CommunityFragment extends Fragment{
         popupWindow.setAnimationStyle(R.style.take_photo_anim);
         View parentView = LayoutInflater.from(getContext()).inflate(R.layout.comment_popupwindow, null);
         popupWindow.showAtLocation(parentView, Gravity.BOTTOM, 0, 0);
+        //点击叉号按钮
         iv_delete=popupWindowView.findViewById( R.id.iv_delete);
         iv_delete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -240,14 +137,4 @@ public class CommunityFragment extends Fragment{
 
 
     }
-
-
-
-
-
-
-
-
-
-
 }
