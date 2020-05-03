a805e4d6c9603eab0b7781f30e9e341bb3bdf870
FuShaoLei <1563250958@qq.com>
2020-05-03 17:40:18 +0800

    写了一些游戏的界面

---
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/GameActivity.java
@@ -0,0 +1,14 @@
+package com.example.a15632.poetrydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();//去掉顶部标题栏
+        setContentView(R.layout.activity_game);
+    }
+}
--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/GameFragment.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/GameFragment.java
@@ -1,5 +1,6 @@
 package com.example.a15632.poetrydemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,9 +8,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class GameFragment extends Fragment {
     private View fragment;
+    private Button intoGame;
+    private LinearLayout paihang;
 
     @Nullable
     @Override
@@ -19,12 +24,8 @@ public class GameFragment extends Fragment {
         fragment=inflater.inflate(R.layout.tab_game_layout,container,false);
 
         //code begin
-
-
-
-
-
-
+        findView();
+        action();
 
         //code end
         ViewGroup p=(ViewGroup)fragment.getParent();
@@ -33,5 +34,26 @@ public class GameFragment extends Fragment {
         }
 
         return fragment;
+    }
+
+    private void findView() {
+        intoGame=fragment.findViewById(R.id.btn_into_game);
+        paihang=fragment.findViewById(R.id.linear_game_paihang);
+    }
+    private void action(){
+        //点击跳转事件
+        intoGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),GameActivity.class));
+            }
+        });
+        //点击弹窗事件
+        paihang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
