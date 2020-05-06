dccab55d123f56bb18fe200946646699315bfecf
hl115 <1154453784@qq.com>
2020-05-06 18:13:29 +0800

    addsend

--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/AddCommunity.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/AddCommunity.java
@@ -1,14 +1,201 @@
 package com.example.a15632.poetrydemo;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.app.AlertDialog;
+
+import com.example.a15632.poetrydemo.util.ImageUtils;
+import com.example.a15632.poetrydemo.util.ScreenUtils;
+
+import java.io.FileNotFoundException;
 
 public class AddCommunity extends AppCompatActivity {
+
+    private EditText title;
+    private  EditText content;
+    private ImageView picture;
+    private TextView tv_type;
+    private ImageView iv_send;
+    private ImageView iv_delete;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();//去掉顶部标题栏
         setContentView(R.layout.addcommunity);
+
+        findViews();
+
+        Intent intent=this.getIntent();
+        int type=intent.getIntExtra("type",0);
+        if(type==1){
+            tv_type.setText("原创诗词");
+        }
+        else if(type==2){
+            tv_type.setText("社区话题");
+        }
+        else{
+            tv_type.setText("发布类型");
+        }
+
+        MyListener myListener=new MyListener();
+        picture.setOnClickListener(myListener);
+        iv_delete.setOnClickListener(myListener);
+        iv_send.setOnClickListener(myListener);
     }
+
+    private void findViews(){
+        title=findViewById(R.id.e_title);
+        content=findViewById(R.id.e_content);
+        picture=findViewById(R.id.iv_image);
+        tv_type=findViewById(R.id.tv_type);
+        iv_send=findViewById(R.id.iv_send);
+        iv_delete=findViewById(R.id.iv_back);
+    }
+
+
+    private class MyListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.iv_image:
+                    ActivityCompat.requestPermissions(AddCommunity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
+                    break;
+                case R.id.iv_back:
+                    new AlertDialog.Builder(AddCommunity.this)
+                            .setTitle("您确定退出发布吗?")
+                            .setMessage("您的发布将保存，可在我的草稿内进行查看。")
+                            .setPositiveButton("退出发布",new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // TODO Auto-generated method stub
+                                    finish();
+                                }
+                            })
+                            .setNegativeButton("继续发布", null)
+                            .show();
+                    break;
+                case R.id.iv_send:
+                    new AlertDialog.Builder(AddCommunity.this)
+                            .setTitle("您确定发布吗？")
+                            .setPositiveButton("确定发布",new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // TODO Auto-generated method stub
+                                    finish();
+                                }
+                            })
+                            .setNegativeButton("返回修改", null)
+                            .show();
+
+            }
+        }
+    }
+
+    //用户允许权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100){
+            Intent intent=new Intent();
+            intent.setAction(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent,200);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bm = null;
+        // 外界的程序访问ContentProvider所提供数据 可以通过ContentResolver接口
+        ContentResolver resolver = getContentResolver();
+        if (requestCode == 200 && resultCode == RESULT_OK) {
+            try{
+                // 获得图片的uri
+                Uri originalUri = data.getData();
+                //bm = MediaStore.Images.Media.getBitmap(resolver,originalUri);
+                // String[] proj = {MediaStore.Images.Media.DATA};
+                // 好像是android多媒体数据库的封装接口，具体的看Android文档
+                // Cursor cursor = managedQuery(originalUri,proj,null,null,null);
+                Cursor cursor = getContentResolver().query(originalUri, null, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if(cursor.moveToFirst()){
+                    String path = cursor.getString(cursor.getColumnIndex("_data"));
+                    insertImg(path);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                Toast.makeText(AddCommunity.this,"图片插入失败",Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+    }
+
+    private void insertImg(String path){
+        String tagPath = "<img src=\""+path+"\"/>";//为图片路径加上<img>标签
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        if(bitmap != null){
+            SpannableString ss = getBitmapMime(path,tagPath);
+            insertPhotoToEditText(ss);
+            content.append("\n");
+        }
+    }
+    //endregion
+
+    //region 将图片插入到EditText中
+    private void insertPhotoToEditText(SpannableString ss){
+        Editable et = content.getText();
+        int start = content.getSelectionStart();
+        et.insert(start,ss);
+        content.setText(et);
+        content.setSelection(start+ss.length());
+        content.setFocusableInTouchMode(true);
+        content.setFocusable(true);
+    }
+    //endregion
+
+    private SpannableString getBitmapMime(String path, String tagPath) {
+        SpannableString ss = new SpannableString(tagPath);//这里使用加了<img>标签的图片路径
+
+        int width = ScreenUtils.getScreenWidth(AddCommunity.this);
+        int height = ScreenUtils.getScreenHeight(AddCommunity.this);
+
+        Bitmap bitmap = ImageUtils.getSmallBitmap(path,width,480);
+        ImageSpan imageSpan = new ImageSpan(this, bitmap);
+        ss.setSpan(imageSpan, 0, tagPath.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return ss;
+    }
+
+
+
+
+
+
+
 }
--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/CommunityFragment.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/CommunityFragment.java
@@ -2,6 +2,7 @@ package com.example.a15632.poetrydemo;
 
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -26,58 +27,63 @@ import java.util.ArrayList;
 
 import java.util.List;
 
-public class CommunityFragment extends Fragment{
-    private  ViewPager mViewPager;
+public class CommunityFragment extends Fragment {
+    private ViewPager mViewPager;
     private View fragment;
     private TabLayout mTabLayout;
 
     private TitleLayout titleLayout;
-    private ImageView iv_delete=null;
+    private ImageView iv_delete = null;
 
     List<Fragment> mFragmentList = new ArrayList<Fragment>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        fragment=inflater.inflate(R.layout.tab_community_layout,container,false);
+        fragment = inflater.inflate(R.layout.tab_community_layout, container, false);
         //code begin
         findViews();
         initViews();
-        MyListener myListener=new MyListener();
+        MyListener myListener = new MyListener();
         //发布按钮
         titleLayout.setRightIconOnClickListener(myListener);
         //code end
-        ViewGroup p=(ViewGroup)fragment.getParent();
-        if(p!=null){
+        ViewGroup p = (ViewGroup) fragment.getParent();
+        if (p != null) {
             p.removeView(fragment);
         }
 
         return fragment;
     }
 
-    public void findViews(){
-        mViewPager=(ViewPager) fragment.findViewById(R.id.viewpager);
-        mTabLayout=(TabLayout) fragment.findViewById(R.id.tabLayout);
-        titleLayout=fragment.findViewById(R.id.title_bar);
+    public void findViews() {
+        mViewPager = (ViewPager) fragment.findViewById(R.id.viewpager);
+        mTabLayout = (TabLayout) fragment.findViewById(R.id.tabLayout);
+        titleLayout = fragment.findViewById(R.id.title_bar);
     }
-    private void initViews(){
-        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getChildFragmentManager());
-        viewPagerAdapter.AddFragment(new AddPoemFragment(),"原创诗词");
-        viewPagerAdapter.AddFragment(new AddTalkFragment(),"社区话题");
+
+    private void initViews() {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+        viewPagerAdapter.AddFragment(new AddPoemFragment(), "原创诗词");
+        viewPagerAdapter.AddFragment(new AddTalkFragment(), "社区话题");
+        viewPagerAdapter.AddFragment(new AddPoemFragment(), "我的关注");
         mViewPager.setAdapter(viewPagerAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
     }
-    private class MyListener implements View.OnClickListener{
+
+    private class MyListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            switch(v.getId()){
+            switch (v.getId()) {
                 case R.id.addcommunity:
                     // 弹出PopupMenu
                     showPopupMenu(v);
             }
         }
     }
+
     private void showPopupMenu(View v) {
         PopupMenu popupMenu
                 = new PopupMenu(fragment.getContext(), v);
@@ -90,12 +96,16 @@ public class CommunityFragment extends Fragment{
                         Toast.makeText(fragment.getContext(),
                                 item.getTitle(),
                                 Toast.LENGTH_SHORT).show();
-                        switch (item.getItemId()){
+                        switch (item.getItemId()) {
                             case R.id.item_addpoem:
-                                showPopupWindow(fragment);
+                                Intent intent = new Intent(getActivity(), AddCommunity.class);
+                                intent.putExtra("type", 1);
+                                startActivity(intent);
                                 break;
                             case R.id.item_addtalk:
-                                showPopupWindow(fragment);
+                                Intent intent1 = new Intent(getActivity(), AddCommunity.class);
+                                intent1.putExtra("type", 2);
+                                startActivity(intent1);
                                 break;
                         }
                         return false;
@@ -105,36 +115,5 @@ public class CommunityFragment extends Fragment{
         popupMenu.show();
 
     }
-    //显示弹窗
-    private void showPopupWindow(View v){
-        View popupWindowView=getLayoutInflater().inflate(R.layout.addcommunity,null);
-        final PopupWindow popupWindow=new PopupWindow(popupWindowView, ActionBar.LayoutParams.MATCH_PARENT ,ActionBar.LayoutParams.MATCH_PARENT, true);
-        WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
-        lp.alpha = 0.7f;
-        getActivity().getWindow().setAttributes(lp);
-        //dismiss时恢复原样
-        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
-            @Override
-            public void onDismiss() {
-                WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
-                lp.alpha = 1f;
-                getActivity().getWindow().setAttributes(lp);
-            }
-        });
-        //弹出动画
-        popupWindow.setAnimationStyle(R.style.take_photo_anim);
-        View parentView = LayoutInflater.from(getContext()).inflate(R.layout.comment_popupwindow, null);
-        popupWindow.showAtLocation(parentView, Gravity.BOTTOM, 0, 0);
-        //点击叉号按钮
-        iv_delete=popupWindowView.findViewById( R.id.iv_delete);
-        iv_delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(),"返回",Toast.LENGTH_SHORT).show();
-                popupWindow.dismiss();
-            }
-        });
 
-
-    }
 }
