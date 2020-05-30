6d845273091ae113b23c663bc419331a724a7b00
yanghuiaaa <1325655415@qq.com>
2020-05-30 20:14:49 +0800

    调整

--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/drawer/AboutActivity.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/drawer/AboutActivity.java
@@ -1,16 +1,33 @@
 package com.example.a15632.poetrydemo.drawer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.a15632.poetrydemo.R;
 
 public class AboutActivity extends AppCompatActivity {
-
+    private ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();//去掉顶部标题栏
         setContentView(R.layout.activity_about);
+
+        back=findViewById(R.id.img_about_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
+
+
+
+
 }
