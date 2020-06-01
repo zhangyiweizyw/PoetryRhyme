88a14452823d0c41ef09afbde07cf5b9a2c4ca1b
yanghuiaaa <1325655415@qq.com>
2020-06-01 12:22:12 +0800

    y

--- PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/SystemMessage.java
+++ PoetryDemo/app/src/main/java/com/example/a15632/poetrydemo/SystemMessage.java
@@ -42,7 +42,7 @@ public class SystemMessage extends AppCompatActivity {
     }
 
     private void initViews(){
-        SystemMsg systemMsg=new SystemMsg("重磅消息", R.drawable.chinapoem,"诗韵官方将和中央电视台《中国诗词大会》节目组联合举办第一届线上诗词大会！届时，不仅有来自天南地北的小伙伴们共同切磋诗词，更有董卿老师，康震老师，蒙曼老师等线上亲切指导。本活动旨在传扬中国传统优秀诗词文化，诚邀小伙伴们积极参加！");
+        SystemMsg systemMsg=new SystemMsg("重磅消息", R.drawable.shici3,"诗韵官方将和《中国诗词大会》节目组联合举办第一届线上诗词大会！届时，不仅有来自天南地北的小伙伴们共同切磋诗词，还可以进行诗词交流探讨，本活动旨在传扬中国传统优秀诗词文化，诚邀小伙伴们积极参加！");
         SystemMsg systemMsg1=new SystemMsg("我与诗词征文比赛", R.drawable.img4,"诗韵官方将于六月十号开展“我与诗词”征文比赛。小伙伴们可以尽情书写你与诗词之间的故事。文体不限，1500字以上。希望大家踊跃参加，官方为大家准备了丰厚的礼品哦！");
         systemMsgs.add(systemMsg);
         systemMsgs.add(systemMsg1);
